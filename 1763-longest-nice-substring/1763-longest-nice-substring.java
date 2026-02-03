2c7ba34acfa3ed1a8bd02682c10225622e0c2637
ASHWANI-MIS <129867430+ASHWANI-MIS@users.noreply.github.com>
2026-02-03 21:57:28 +0530

    Time: 70 ms (9.47%), Space: 46.8 MB (20.73%) - LeetHub

---
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String longestNiceSubstring(String s) {
+        int n = s.length();
+        String ans = "";
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                String sub = s.substring(i, j);
+                if (isNice(sub) && sub.length() > ans.length()) {
+                    ans = sub;
+                }
+            }
+        }
+        return ans;
+    }
+
+    private boolean isNice(String s) {
+        java.util.Set<Character> set = new java.util.HashSet<>();
+        for (char c : s.toCharArray()) {
+            set.add(c);
+        }
+
+        for (char c : set) {
+            if (!set.contains(Character.toLowerCase(c)) ||
+                !set.contains(Character.toUpperCase(c))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
