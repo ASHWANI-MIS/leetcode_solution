b2d838550590179a04c53936faeca0d6d9dc74aa
ASHWANI-MIS <129867430+ASHWANI-MIS@users.noreply.github.com>
2026-02-03 22:10:02 +0530

    Time: 28 ms (21.22%), Space: 46.7 MB (20.73%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.java
+++ 1763-longest-nice-substring/1763-longest-nice-substring.java
@@ -1,10 +1,40 @@
+//class Solution {
+   // public String longestNiceSubstring(String s) {
+     //   int n = s.length();
+       // String ans = "";
+
+        //for (int i = 0; i < n; i++) {
+          //  for (int j = i + 1; j <= n; j++) {
+            //    String sub = s.substring(i, j);
+              //  if (isNice(sub) && sub.length() > ans.length()) {
+                //    ans = sub;
+                //}
+            //}
+        //}
+        //return ans;
+    //}
+
+   // private boolean isNice(String s) {
+     //   java.util.Set<Character> set = new java.util.HashSet<>();
+       // for (char c : s.toCharArray()) {
+         //   set.add(c);
+        //}
+
+       // for (char c : set) {
+         //   if (!set.contains(Character.toLowerCase(c)) ||
+           //     !set.contains(Character.toUpperCase(c))) {
+             //   return false;
+            //}
+        //}
+        //return true;
+    //}
+//}
 class Solution {
     public String longestNiceSubstring(String s) {
-        int n = s.length();
         String ans = "";
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j <= n; j++) {
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
                 String sub = s.substring(i, j);
                 if (isNice(sub) && sub.length() > ans.length()) {
                     ans = sub;
@@ -14,18 +44,14 @@ class Solution {
         return ans;
     }
 
-    private boolean isNice(String s) {
-        java.util.Set<Character> set = new java.util.HashSet<>();
+    boolean isNice(String s) {
         for (char c : s.toCharArray()) {
-            set.add(c);
-        }
-
-        for (char c : set) {
-            if (!set.contains(Character.toLowerCase(c)) ||
-                !set.contains(Character.toUpperCase(c))) {
+            if (s.indexOf(Character.toLowerCase(c)) == -1 ||
+                s.indexOf(Character.toUpperCase(c)) == -1) {
                 return false;
             }
         }
         return true;
     }
 }
+
